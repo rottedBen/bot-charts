40378fc02db20fd76e94f249ad000f8bc5892ccd
sharkgua <benoit.george@ubirch.com>
2020-10-09 17:53:09 +0200

    init

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ def get_from_query(query_received):
         k_hours = time_start
     if time_type == 'd' or time_type == 'D':
         k_days = time_start
-    return time_type, time_start, k_hours, k_days
+    return time_type, k_hours, k_days
 
 
 def strp_date(raw_date):
@@ -68,24 +68,24 @@ def delete_line_from_file(path, msg):
 
 
 def check_query(query_received):
-    time_type, time_start, k_hours, k_days, tokens = 'd', 1, 0, 1, "ROT"
+    time_type, k_hours, k_days, tokens = 'd', 1, 0, 1, "ROT"
     if len(query_received) == 1:
         pass
     elif len(query_received) == 2:
         tokens = [query_received[1]]
     elif len(query_received) == 3:
-        time_type, time_start, k_hours, k_days = get_from_query(query_received)
+        time_type, k_hours, k_days = get_from_query(query_received)
     elif len(query_received) == 4:
-        time_type, time_start, k_hours, k_days = get_from_query(query_received)
+        time_type, k_hours, k_days = get_from_query(query_received)
         tokens = [query_received[-1]]
     else:
-        time_type, time_start, k_hours, k_days = get_from_query(query_received)
+        time_type, k_hours, k_days = get_from_query(query_received)
         tokens = query_received[3:]
     return time_type, k_hours, k_days, tokens
 
 
 def check_query_fav(query_received):
-    time_type, k_hours, k_days = 'd', 1, 0, 1
+    time_type, k_hours, k_days = 'd', 0, 1
     if len(query_received) == 1:
         pass
     elif len(query_received) == 2:
